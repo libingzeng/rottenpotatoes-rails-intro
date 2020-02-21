1ed153b31d813903fb1875a02f41eaee5709f118
libingzeng <ec2-user@ip-172-31-41-48.us-east-2.compute.internal>
2020-02-21 04:28:59 +0000

    test

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,13 +6,13 @@ class MoviesController < ApplicationController
 
   def show
     id = params[:id] # retrieve movie ID from URI route
-    puts "----------------------id is #{id}"
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
 
   def index
-    @movies = Movie.all
+    #@movies = Movie.all
+    @movies = Movie.order(:title)
   end
 
   def new
