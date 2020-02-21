9bb721c989ba4d508ff11ef5dcd33681b4fb999b
libingzeng <ec2-user@ip-172-31-41-48.us-east-2.compute.internal>
2020-02-21 04:10:27 +0000

    test

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,41 +1,35 @@
 class MoviesController < ApplicationController
 
   def movie_params
-    puts "controller......................movie_params....................."
     params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
 
   def show
     id = params[:id] # retrieve movie ID from URI route
-    puts "controller......................show....................."
+    puts "----------------------id is #{id}"
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
 
   def index
     @movies = Movie.all
-    puts "controller......................index....................."
   end
 
   def new
-    puts "controller......................new....................."
     # default: render 'new' template
   end
 
   def create
-    puts "controller......................create....................."
     @movie = Movie.create!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
 
   def edit
-    puts "controller......................edit....................."
     @movie = Movie.find params[:id]
   end
 
   def update
-    puts "controller......................update....................."
     @movie = Movie.find params[:id]
     @movie.update_attributes!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully updated."
@@ -43,7 +37,6 @@ class MoviesController < ApplicationController
   end
 
   def destroy
-    puts "controller......................destroy....................."
     @movie = Movie.find(params[:id])
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
