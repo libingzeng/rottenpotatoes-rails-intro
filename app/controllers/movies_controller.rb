e3bd0ea2964adb9102f32aa1702147ceebb79f35
libingzeng <ec2-user@ip-172-31-41-48.us-east-2.compute.internal>
2020-02-21 04:56:47 +0000

    test

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,7 +12,8 @@ class MoviesController < ApplicationController
 
   def index
     #@movies = Movie.all
-    @movies = Movie.order(title: :desc)
+    #@movies = Movie.order(:title) #ascending order
+    @movies = Movie.order(release_date: :desc)#decreasing order
   end
 
   def new
