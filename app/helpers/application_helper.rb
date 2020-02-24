d0f30a6e7b156f189585955de53adc3dc8e76afd
libingzeng <ec2-user@ip-172-31-41-48.us-east-2.compute.internal>
2020-02-24 04:39:06 +0000

    test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  def sortable(column, title=nil, selected_ratings)
+    title ||= column.titleize
+        
+    if column == sort_column && sort_direction == "asc"
+        direction = "desc"
+    else direction = "asc"
+    end
+
+    if column == sort_column
+        css_class = "hilite"
+    else css_class = nil
+    end
+
+    link_to title, {:sort => column, :direction => direction, 
+      :ratings => @selected_ratings}, {:class => css_class}
+  end   
+
+  def checked?(rating)
+    checked_ratings.has_key?(rating) ? true : nil
+  end
 end
