1593ac7cd35a8b2856eb98b3b330bbb683cba96d
libingzeng <ec2-user@ip-172-31-41-48.us-east-2.compute.internal>
2020-02-28 04:20:14 +0000

    test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,13 +4,14 @@ module ApplicationHelper
     title ||= column.titleize
         
     if column == sort_column && sort_direction == "asc"
-        direction = "desc"
+      direction = "desc"
     else direction = "asc"
     end
 
     if column == sort_column
-        css_class = "hilite"
-    else css_class = nil
+      css_class = "hilite"
+    else 
+      css_class = nil
     end
 
     link_to title, {:sort => column, :direction => direction}, {:class => css_class}
