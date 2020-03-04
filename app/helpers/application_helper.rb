bcc53bc4b3b2b14997a8043b1b089bdd32fcdc6f
libingzeng <ec2-user@ip-172-31-41-48.us-east-2.compute.internal>
2020-03-04 22:57:43 +0000

    part 3 complete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,3 @@
 module ApplicationHelper
     
-  def sortable(column, title=nil)
-    title ||= column
-        
-    if column == sort_column && sort_direction == "asc"
-      direction = "desc"
-    else direction = "asc"
-    end
-
-    if column == sort_column
-      css_class = "hilite"
-    else 
-      css_class = "original"
-    end
-
-    link_to title, {:sort => column, :direction => direction}, {:class => css_class}
-  end   
-
 end
